3dae2529ea1e419a3e07dac24015176c21042044
kaylamf25 <46701855+kaylamf25@users.noreply.github.com>
2019-04-21 19:16:15 -0500

    Add files via upload

---
+++ CozmoMusicalBlocks.py
@@ -0,0 +1,97 @@
+import cozmo
+import socket
+import errno
+import time
+import random
+import pygame
+from socket import error as socket_error
+import asyncio
+import collections
+import math
+import warnings
+from cozmo.util import degrees, distance_mm, speed_mmps
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket_error as msg:
+        robot.say_text("socket failed" + msg).wait_for_completed()
+    ip = "10.0.1.10"
+    port = 5000
+    
+    try:
+        s.connect((ip, port))
+    except socket_error as msg:
+        robot.say_text("socket failed to bind").wait_for_completed()
+    cont = True
+    
+    name = "Playing"
+    
+    while cont:
+        robot.set_lift_height(0).wait_for_completed()
+        hasBlock = False
+        cubes = None
+        cozmo.robot.Robot.start_freeplay_behaviors(robot)
+        bytedata = s.recv(4048)
+        data = bytedata.decode('utf-8')
+        print(str(data))
+        if not data:
+            cont = False
+            s.close()
+            quit()
+        else:
+            cozmo.robot.Robot.stop_freeplay_behaviors(robot)
+            print(data)
+            instructions = data.split(';')
+            if instructions[0] == name:
+                print("In name")
+                hasBlock = False
+                if len(instructions) == 2:  
+                    print("Correct length")
+                    if instructions[1] == 1:
+                        print("1")
+                        while not hasBlock:
+                            print("In while loop")
+                            lookaround = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+                            cubes = robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.LightCube, timeout=60)
+                            lookaround.stop()
+                            current_action = robot.pickup_object(cubes[0], num_retries=3)
+                            current_action.wait_for_completed()
+                            if current_action.has_failed:
+                                code, reason = current_action.failure_reason
+                                result = current_action.result
+                                print("Pickup Cube failed: code=%s reason='%s' result=%s" % (code, reason, result))
+                                cubes = None
+                                return
+                            else:    
+                                print("Worked")
+                                hasBlock = True                                                
+                    elif instructions[1] == 2:
+                        #triggers cozmo to celebrate if they have a block or get sad if he doesn't have one
+                        if(hasBlock == True):
+                            robot.set_lift_height(0).wait_for_completed()
+                            robot.drive_straight(cozmo.util.distance_mm(-100), cozmo.util.speed_mmps(100)).wait_for_completed()                            
+                            robot.play_anim_trigger(cozmo.anim.Triggers.OnSpeedtapGameCozmoWinHighIntensity).wait_for_completed()
+                        else:
+                            robot.set_lift_height(0).wait_for_completed()                            
+                            robot.play_anim_trigger(cozmo.anim.Triggers.OnSpeedtapGamePlayerWinHighIntensity).wait_for_completed()
+                            name = "Out"
+                        return
+                    elif instructions[1] == 3:
+                        #sent if only 1 block was to be found to declare winner
+                        if(hasBlock == True):
+                            robot.set_lift_height(0).wait_for_completed()
+                            robot.drive_straight(cozmo.util.distance_mm(-100), cozmo.util.speed_mmps(100)).wait_for_completed()                            
+                            robot.play_anim_trigger(cozmo.anim.Triggers.OnSpeedtapGameCozmoWinHighIntensity).wait_for_completed()                        
+                            robot.say_text("Winner").wait_for_completed()
+                        else:
+                            robot.set_lift_height(0).wait_for_completed()                            
+                            robot.play_anim_trigger(cozmo.anim.Triggers.OnSpeedtapGamePlayerWinHighIntensity).wait_for_completed()
+                            name = "Out"                        
+                
+                
+                
+                
+                
+cozmo.run_program(cozmo_program)
---
+++ MasterMusicalBlocks.py
@@ -0,0 +1,68 @@
+import socket
+import errno
+import time
+import random
+import pygame
+from socket import error as socket_error
+import asyncio
+import collections
+import math
+import warnings
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket_error as msg:
+    print("socket failed" + msg).wait_for_completed()
+ip = "10.0.1.10"
+port = 5000
+ 
+try:
+    s.connect((ip, port))
+except socket_error as msg:
+    print("socket failed to bind").wait_for_completed()
+cont = True  
+
+roundNumber = 1
+blocksFound = 0
+listen = True
+
+waitTime = random.randint(5,15)
+print(waitTime)
+time.sleep(waitTime)
+s.sendall(b'Playing;1') #start looking
+while roundNumber > 0:
+    numOfCozmos = int(input("Enter the number of Cozmos playing: "))
+    numOfBlocks = int(input("Enter the number of blocks this round: ")) 
+    print("Begin")
+    waitTime = random.randint(10,45)
+    startTime = random.randint(0, 211-waitTime)
+    print("Wait time: ",waitTime)    
+    print("Start time: ",startTime)
+    pygame.mixer.init()
+    pygame.mixer.music.load('The_Hampster_Dance.mp3') #211 seconds long
+    pygame.mixer_music.play(start=startTime)
+    time.sleep(waitTime)
+    s.sendall(b'Playing;1') #start looking
+    pygame.mixer_music.stop()    
+    if listen == True:
+        bytedata = s.recv(4048)
+        data = bytedata.decode('utf-8')
+        print(data)
+        instructions = data.split(';')
+        if instructions[0] == 'BlockFound': #if a cozmo finds a block it should get into here
+            blocksFound = blocksFound + 1            
+    else:
+        listen = True
+        
+    if blocksFound == numOfBlocks:
+        s.sendall(b'Playing;2') #post game celebration or sad and name change
+        if(blocksFound == 1):
+            s.sendall(b'Playing;3')
+            roundNumber = 0;
+            return
+        else:
+            blocksFound = 0; #set the amount of blocks found back to 0 for the next round
+            roundNumber = roundNumber + 1
+        continue
+
+
