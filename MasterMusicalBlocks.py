a7a5d3c1ea5d8b7a5ddb957f25dab3f445c70b9c
kaylamf25 <46701855+kaylamf25@users.noreply.github.com>
2019-05-01 10:03:33 -0500

    Update MasterMusicalBlocks.py

--- MasterMusicalBlocks.py
+++ MasterMusicalBlocks.py
@@ -26,51 +26,67 @@ roundNumber = 1
 blocksFound = 0
 listen = True
 
-waitTime = random.randint(5,15)
-print(waitTime)
-time.sleep(waitTime)
-s.sendall(b'Playing;1') #start looking
-
 while roundNumber > 0:
     numOfCozmos = int(input("Enter the number of Cozmos playing: "))
     numOfBlocks = int(input("Enter the number of blocks this round: ")) 
     readyCount = 0;
-    while(readyCount<numOfCozmos):
+    print('1st stop')
+    while readyCount<numOfCozmos and (roundNumber == 1):
+        print('in while')
         s.sendall(b'Ready?')  
         bytedata = s.recv(4048)
         data = bytedata.decode('utf-8')
         print(data)
         instructions = data.split(';')
         if instructions[0] == 'Ready': #if a cozmo finds a block it should get into here
-            readyCount = readyCount + 1          
+            readyCount = readyCount + 1 
+        elif instructions[0] == 'ReadyReady':
+            readyCount = readyCount + 2
     print("Begin")
-    waitTime = random.randint(10,45)
+    waitTime = random.randint(10,20)
     startTime = random.randint(0, 211-waitTime)
     print("Wait time: ",waitTime)    
     print("Start time: ",startTime)
     pygame.mixer.init()
     pygame.mixer.music.load('The_Hampster_Dance.mp3') #211 seconds long
+    s.sendall(b'Music')
     pygame.mixer_music.play(start=startTime)
     time.sleep(waitTime)
-    s.sendall(b'Playing;1') #start looking
-    pygame.mixer_music.stop()    
-    if listen == True:
+    s.sendall(b'Look') #start looking
+    pygame.mixer_music.stop()
+    
+    while (not(blocksFound==numOfBlocks)):
+        if listen == True:
+            bytedata = s.recv(4048)
+            data = bytedata.decode('utf-8')
+            print(data)
+            instructions = data.split(';')
+            if instructions[0] == 'BlockFound': #if a cozmo finds a block it should get into here
+                blocksFound = blocksFound + 1            
+            else:
+                listen = True
+                s.sendall(b'Look')
+        
+    listenCount = 0
+    while listenCount<numOfCozmos:
+        s.sendall(b'Listening?')  
         bytedata = s.recv(4048)
         data = bytedata.decode('utf-8')
         print(data)
         instructions = data.split(';')
-        if instructions[0] == 'BlockFound': #if a cozmo finds a block it should get into here
-            blocksFound = blocksFound + 1            
+        if instructions[0] == 'Listening': 
+            listenCount = listenCount + 1 
+        elif instructions[0] == 'ListeningListening':
+            listenCount = listenCount + 2    
+    
+    listenCount = 0
+    if(numOfBlocks == 1):
+        time.sleep(1)
+        s.sendall(b'GameOver')
+        roundNumber = 0    
+        blocksFound = 0
     else:
-        listen = True
-        
-    if blocksFound == numOfBlocks:
-        s.sendall(b'Playing;2') #post game celebration or sad and name change
-        if(blocksFound == 1):
-            s.sendall(b'Playing;3')
-            roundNumber = 0;
-            return
-        else:
-            blocksFound = 0; #set the amount of blocks found back to 0 for the next round
-            roundNumber = roundNumber + 1
-        continue
+        time.sleep(1)
+        s.sendall(b'RoundOver') #post game celebration or sad and name change3  
+        blocksFound = 0 #set the amount of blocks found back to 0 for the next round
+        roundNumber = roundNumber + 1
