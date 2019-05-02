763b9c1adaf8f13eac0acd65d4b5b2ebc8b5c64d
kaylamf25 <46701855+kaylamf25@users.noreply.github.com>
2019-05-01 19:53:58 -0500

    Update CozmoMusicalBlocks.py

--- CozmoMusicalBlocks.py
+++ CozmoMusicalBlocks.py
@@ -77,7 +77,7 @@ def cozmo_program(robot: cozmo.robot.Robot):
                 lookaround = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
                 cubes = robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.LightCube, timeout=20)
                 lookaround.stop()
-                if (not(cubes == None)) or (not(len(cubes) ==0)):
+                if (not(len(cubes) ==0)):
                     current_action = robot.pickup_object(cubes[0], num_retries=1)
                     current_action.wait_for_completed()
                     if current_action.has_failed:
